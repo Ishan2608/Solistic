5ee57f2f81ad46035b96532390055de7403f364c
ishanrastogi26 <ishanrastogi26@gmail.com>
2025-04-28 13:19:30 +0000

    Set Starter Navigation Style

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,26 +4,39 @@ import { Link } from "react-router-dom";
 function Navigation() {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/auth">Auth</Link>
-        </li>
-        <li>
-          <Link to="/news">News</Link>
-        </li>
-        <li>
-          <Link to="/image-gallery">Image Gallery</Link>
-        </li>
-        <li>
-          <Link to="/solar-system-simulator">Solary System Simulator</Link>
-        </li>
-        <li>
-          <Link to="/tutor">Tutor</Link>
-        </li>
-      </ul>
+      {/* Navigation Logo */}
+      <div>
+        <img src="/logo.png" alt="Logo" />
+      </div>
+
+      {/* Navigation Links */}
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/auth">Auth</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/image-gallery">Image Gallery</Link>
+          </li>
+          <li>
+            <Link to="/solar-system-simulator">Solary System Simulator</Link>
+          </li>
+          <li>
+            <Link to="/tutor">Tutor</Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Sign-Up Button */}
+      <div>
+        <button>Sign Up/Login</button>
+      </div>
     </nav>
   );
 }
