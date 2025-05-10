d30c1a94872dd0d2b97cb3ba07cca77293f4e13c
ishanrastogi26 <ishanrastogi26@gmail.com>
2025-05-10 16:16:57 +0000

    Header Text Updates

--- src/pages/Tutor.jsx
+++ src/pages/Tutor.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Header from "../components/Common/Header";
 
-const HEADER_TITLE = "Welcome to अंतरिक्ष Tutor";
-const HEADER_PARA = "Knowledge is power. Only we can give you that power";
+const HEADER_TITLE = "अंतरिक्ष Tutor Says Hi";
+const HEADER_PARA = "Knowledge is power. Only we can give you that power.";
 const HEADER_BG_URL = "https://images.unsplash.com/photo-1537819191377-d3305ffddce4?q=80&w=1421&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
 const Tutor = () =>{
